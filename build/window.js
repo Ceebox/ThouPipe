414c442845dc2ea0ca7c52de0f79bcc9da8fb29a
ceebox <chadderboxdev@gmail.com>
2023-10-20 23:40:22 +0000

    Automatically built unbuilt typescript

--- build/window.js
+++ build/window.js
@@ -1,3 +1,4 @@
+import { Input } from "./input.js";
 import { JSONHelper } from "./jsonhelper.js";
 var Window = (function () {
     function Window() {
@@ -16,17 +17,17 @@ var Window = (function () {
             this.video.pause();
         }
     };
-    Window.prototype.pause = function () {
-        if (this.video === undefined || this.video.duration === Number.NaN) {
-            return;
-        }
-        this.video.pause();
-    };
     Window.prototype.play = function () {
         if (this.video === undefined || this.video.duration === Number.NaN) {
             return;
         }
         this.video.play();
+    };
+    Window.prototype.pause = function () {
+        if (this.video === undefined || this.video.duration === Number.NaN) {
+            return;
+        }
+        this.video.pause();
     };
     Window.prototype.toggleMute = function () {
         if (this.video === undefined || this.video.duration === Number.NaN) {
@@ -34,11 +35,31 @@ var Window = (function () {
         }
         this.video.muted = !this.video.muted;
     };
+    Window.prototype.skipBack = function () {
+        this.seekTime(this.video.currentTime - 1);
+    };
+    Window.prototype.skipForward = function () {
+        this.seekTime(this.video.currentTime + 1);
+    };
+    Window.prototype.addPausePoint = function () {
+        this.addTimeData(this.video.currentTime);
+    };
+    Window.prototype.downloadTimeData = function () {
+        var serialised = JSON.stringify(this.timeData);
+        var downloadJSON = new File(["\ufeff" + serialised], this.videoFile.name.replace(".mp4", "") + ".json", { type: "text/plain:charset=UTF-8" });
+        var downloadURL = window.URL.createObjectURL(downloadJSON);
+        var downloadElement = document.createElement("a");
+        downloadElement.href = downloadURL;
+        downloadElement.download = this.videoFile.name.replace(".mp4", "") + ".json";
+        downloadElement.click();
+        window.URL.revokeObjectURL(downloadURL);
+    };
     Window.prototype.seekTime = function (time) {
         if (this.video === undefined || this.video.duration === Number.NaN) {
             return;
         }
         this.video.currentTime = time;
+        this.recalculateTimeIndex();
     };
     Window.prototype.getFiles = function () {
         var _this_1 = this;
@@ -68,24 +89,6 @@ var Window = (function () {
         };
         document.getElementsByTagName("body")[0].appendChild(input);
     };
-    Window.prototype.getFirstFileWithTerms = function (list, terms) {
-        for (var i = 0; i < list.length; i++) {
-            for (var t = 0; t < terms.length; t++) {
-                if (list[i].name.split(terms[t]).length > 1) {
-                    return list[i];
-                }
-            }
-        }
-        return null;
-    };
-    Window.prototype.getFirstFileWithTerm = function (list, term) {
-        for (var i = 0; i < list.length; i++) {
-            if (list[i].name.split(term).length > 1) {
-                return list[i];
-            }
-        }
-        return null;
-    };
     Window.prototype.createWindow = function () {
         var _this_1 = this;
         this.video = document.createElement('video');
@@ -105,7 +108,26 @@ var Window = (function () {
         this.hideScrollBars();
         this.video.ontimeupdate = function () { _this_1.onTimeChanged(); };
         addEventListener("resize", function () { _this_1.setVideoSize(); });
-        this.registerKeybinds();
+        var input = new Input(this);
+        input.registerKeybinds();
+    };
+    Window.prototype.getFirstFileWithTerms = function (list, terms) {
+        for (var i = 0; i < list.length; i++) {
+            for (var t = 0; t < terms.length; t++) {
+                if (list[i].name.split(terms[t]).length > 1) {
+                    return list[i];
+                }
+            }
+        }
+        return null;
+    };
+    Window.prototype.getFirstFileWithTerm = function (list, term) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].name.split(term).length > 1) {
+                return list[i];
+            }
+        }
+        return null;
     };
     Window.prototype.hideScrollBars = function () {
         document.documentElement.style.overflow = 'hidden';
@@ -114,63 +136,12 @@ var Window = (function () {
         this.video.width = document.body.clientWidth;
         this.video.height = document.body.clientHeight;
     };
-    Window.prototype.registerKeybinds = function () {
-        var _this_1 = this;
-        document.body.addEventListener('keydown', function (e) {
-            if (e.key === "Enter") {
-                _this_1.togglePause();
-            }
-        });
-        document.body.addEventListener('keydown', function (e) {
-            if (e.key === "ArrowLeft") {
-                _this_1.skipBack();
-            }
-        });
-        document.body.addEventListener('keydown', function (e) {
-            if (e.key === "ArrowRight") {
-                _this_1.skipForward();
-            }
-        });
-        document.body.addEventListener('keydown', function (e) {
-            if (e.key === "m") {
-                _this_1.toggleMute();
-            }
-        });
-        document.body.addEventListener('keydown', function (e) {
-            if (e.key === "t") {
-                _this_1.addTimeData(_this_1.video.currentTime);
-            }
-        });
-        document.body.addEventListener('keydown', function (e) {
-            if (e.key === "t") {
-                _this_1.downloadTimeData();
-            }
-        });
-    };
     Window.prototype.addTimeData = function (newTime) {
         var newTimeData = { time: newTime };
         this.timeData.push(newTimeData);
         this.timeData.sort(function (a, b) { return a.time - b.time; });
         this.recalculateTimeIndex();
         this.timeIndex++;
-    };
-    Window.prototype.downloadTimeData = function () {
-        var serialised = JSON.stringify(this.timeData);
-        var downloadJSON = new File(["\ufeff" + serialised], this.videoFile.name.replace(".mp4", "") + ".json", { type: "text/plain:charset=UTF-8" });
-        var downloadURL = window.URL.createObjectURL(downloadJSON);
-        var downloadElement = document.createElement("a");
-        downloadElement.href = downloadURL;
-        downloadElement.download = this.videoFile.name.replace(".mp4", "") + ".json";
-        downloadElement.click();
-        window.URL.revokeObjectURL(downloadURL);
-    };
-    Window.prototype.skipBack = function () {
-        this.video.currentTime -= 1;
-        this.recalculateTimeIndex();
-    };
-    Window.prototype.skipForward = function () {
-        this.video.currentTime += 1;
-        this.recalculateTimeIndex();
     };
     Window.prototype.recalculateTimeIndex = function () {
         for (var i = 0; i < this.timeData.length; i++) {
